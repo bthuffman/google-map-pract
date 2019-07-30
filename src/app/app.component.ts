8a911e06f599064aebbcd9434d131ede1dfd69ea
Brandon Huffman <bt_huffman@msn.com>
2019-07-30 08:57:14 -0600

    Able to pull a searched loations info using the Places details documentation on https://developers.google.com/places/web-service/details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
-import { MapsAPILoader, MouseEvent, InfoWindowManager } from '@agm/core';
-import { request } from 'http';
+import { MapsAPILoader, MouseEvent } from '@agm/core';
  
 //Component is a type of directive used to associate a template with a class. 
 @Component({
@@ -16,15 +15,12 @@ export class AppComponent
   zoom: number;
   address: string;
   private geoCoder;
-  map;
-  // albuquerque;
-  infowindow;
-  request;
-  value;
+  name: string;
+ 
   //Any directive, component, and element which is part of component template (i.e. a child of the parent) is accessed as ViewChild. If a parent component wants access to a child component then it uses ViewChild or ContentChild. The @ViewChild decorator is a template querying mechanism that is local to the component and cannot inject anything inside the templates of its child or parent components. 
-  // @ViewChild('search')
+  @ViewChild('search')
   // ElementRef is a wrapper around a native element inside of a View. Allows you to use Angular templates and data binding to access DOM elements without reference to the native element.
-  // public searchElementRef: ElementRef;
+  public searchElementRef: ElementRef;
  
   //The Constructor is a default method of the class that is executed when the class is instantiated and ensures proper initialization of fields in the class and its subclasses. Angular or better Dependency Injector (DI) analyzes the constructor parameters and when it creates a new instance by calling new MyClass() it tries to find providers that match the types of the constructor parameters, resolves them and passes them to the constructor. 
  //specifies what the parameters (injectable services) will be. In this case the constructor is asking for an injected instance of mapsAPILoader and ngZoneand their associated type and metadata. 
@@ -42,28 +38,37 @@ export class AppComponent
       this.setCurrentLocation();
       //assign geoCoder a new Geocoder object
       this.geoCoder = new google.maps.Geocoder;
-
-      // let albuquerque = new google.maps.LatLng(35.096887, -106.654439);
-      let infowindow = new google.maps.InfoWindow();
-
-      this.map = new google.maps.Map(
-        document.getElementById('map')
-      );
-      let request = {
-        // location: this.albuquerque,
-        // radius: '50000',
-        type: ['restaurant']
-      };
+      
+      
       //similar to that found in js example but for autocomplete
-      let service = new google.maps.places.PlacesService(this.map)
-      service.nearbySearch(this.request, this.getAddress);
+      let service = new google.maps.places.Autocomplete
+      //targets the html element labeled 'search' 
+      (this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      
+      service.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = service.getPlace();
+ 
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+ 
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.name = place.name;
+          this.zoom = 12;
+          console.log("This is the searched loactions latitude " + this.latitude);
+          console.log("This is the searched locations name " + this.name);
+        });
+      });
     });
   }
-
-  getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-
+ 
   // Get Current Location Coordinates
   private setCurrentLocation() {
     if ('geolocation' in navigator) {
@@ -76,23 +81,20 @@ export class AppComponent
     }
   }
  
-  // markerDragEnd($event: MouseEvent) {
-  //   console.log($event);
-  //   this.latitude = $event.coords.lat;
-  //   this.longitude = $event.coords.lng;
-  //   this.getAddress(this.latitude, this.longitude);
-  // }
+ 
+  markerDragEnd($event: MouseEvent) {
+    console.log("This is event" + $event);
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
  
   getAddress(latitude, longitude) {
     this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
       console.log(results);
-      console.log(status);
+      console.log("This is the status " + status);
+      
       if (status === 'OK') {
-        let randomNumber =this.getRandomInt(10);
-        console.log(randomNumber);
-
-        let place = results[randomNumber];
-        this.createMarker(results[randomNumber]);
         if (results[0]) {
           this.zoom = 12;
           this.address = results[0].formatted_address;
@@ -105,15 +107,5 @@ export class AppComponent
  
     });
   }
-  createMarker (value: number) {
-    var marker = new google.maps.Marker({
-      map: this.map,
-      position: this.value.geometry.location
-    });
-  
-    google.maps.event.addListener(marker, 'click', function() {
-      this.infowindow.setContent(this.place.name);
-      this.infowindow.open(this.map, this);
-    });
-  }
+ 
 }

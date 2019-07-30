28aa177bb7cbfcbc38415f86c39a684a6a2371f0
Brandon Huffman <bt_huffman@msn.com>
2019-07-30 13:27:58 -0600

    Built some of the logic for randomly generating a restaurant.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,7 +40,7 @@ export class AppComponent
  
  //A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive. Used to  to handle any additional initialization tasks.
   ngOnInit() {
-    console.log(this.mapElement)
+   // console.log(this.mapElement)
     //load Places Autocomplete
     this.mapsAPILoader.load().then(() => {
     this.geoCoder = new google.maps.Geocoder;
@@ -50,101 +50,89 @@ export class AppComponent
     
       // Get Current Location Coordinates
   private setCurrentLocation() {
-    console.log(navigator);
+    // console.log(navigator);
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
         this.zoom = 8;
         this.getAddress(this.latitude, this.longitude);
-        console.log(this.latitude);
-        console.log(this.longitude);
+        //console.log(this.latitude);
+       // console.log(this.longitude);
 
-        var currentLocation = {
-          lat:this.latitude,
-          lng:this.longitude
-        }
-        console.log(currentLocation);
-        // This is the latitude and longitude of 10 miles 0.14492753623 
+
         this.setDefaultBounds();
-        
-        // this.searchRestaurants(){
-        //   var map = new google.maps.Map(this.searchElementRef.nativeElement, {center: currentLocation});
-      
-        //   var request = {
-        //     // location: albuquerque,
-        //     // radius: '50000',
-        //     type: ['restaurant']
-        //   };
-      
-        //   let service = new google.maps.places.PlacesService(map);
-        //   service.nearbySearch(request, callback);
-        // };
-      });
+        this.searchRestaurants;
+        });
+      }
     }
-  }
 
   setDefaultBounds(){
-  var defaultBounds = new google.maps.LatLngBounds(
+    var defaultBounds = new google.maps.LatLngBounds(
     //TODO move to two objects and set each to a constant variable (lat/long)
-    {lat:this.latitude-0.14492753623, lng:this.longitude-0.14492753623}, 
-    {lat:this.latitude+0.14492753623, 
-    lng:this.longitude+0.14492753623}) 
-  // Create the search box and link it to the UI element.
-  var input = this.searchElementRef.nativeElement;
-  var searchBox = new google.maps.places.SearchBox(input, {bounds: defaultBounds});
-   // Listen for the event fired when the user selects a prediction and retrieve more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
+    // This is the latitude and longitude of 10 miles 0.14492753623 
+    {
+      lat:this.latitude-0.14492753623, lng:this.longitude-0.14492753623
+    }, 
+    {
+      lat:this.latitude+0.14492753623, 
+    lng:this.longitude+0.14492753623
+    }) 
+    // Create the search box and link it to the UI element.
+    var input = this.searchElementRef.nativeElement;
+    var searchBox = new google.maps.places.SearchBox(input, {bounds: defaultBounds});
+    // Listen for the event fired when the user selects a prediction and retrieve more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
 
-    if (places.length == 0) {
-      console.log("Failed to find place")
-      return;
-    }
-
-    // Clear out the old markers.
-    this.markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    this.markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
+      if (places.length == 0) {
+        console.log("Failed to find place")
         return;
       }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
 
-      // Create a marker for each place.
-      this.markers.push(new google.maps.Marker({
-        // map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
+      // Clear out the old markers.
+      this.markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      this.markers = [];
 
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        this.markers.push(new google.maps.Marker({
+          // map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
     });
-  });
   }
 
   getAddress(latitude, longitude) {
     this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
-      console.log(results);
-      console.log("This is the status " + status);
+      //console.log(results);
+      //console.log("This is the status " + status);
       
       if (status === 'OK') {
         if (results[0]) {
@@ -156,13 +144,60 @@ export class AppComponent
       } else {
         window.alert('Geocoder failed due to: ' + status);
       }
-      console.log(this.address);
+      //console.log(this.address);
     });
   }
   markerDragEnd($event: MouseEvent) {
-    console.log($event);
+    // console.log($event);
     this.latitude = $event.coords.lat;
     this.longitude = $event.coords.lng;
     this.getAddress(this.latitude, this.longitude);
   }
+
+  searchRestaurants(){
+    var map = new google.maps.Map(this.searchElementRef.nativeElement, 
+      {center: {          
+        lat:this.latitude,
+        lng:this.longitude
+        }
+      });
+    //console.log({lat:this.latitude,
+      //lng:this.longitude});
+    var request = {
+      // location: albuquerque,
+      // radius: '50000',
+      keyword: "restaurant"
+      };
+
+    let service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+    }
+
+    callback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+      
+      let randomNumber = Math.floor(Math.random() * Math.floor(10));
+      console.log(randomNumber);
+      
+      let place = results[randomNumber];
+
+      this.latitude = results.coords.lat;
+      this.longitude = results.coords.lng;
+      this.getAddress(this.latitude, this.longitude);
+      // createMarker(results[randomNumber]);
+      console.log(this.latitude);
+      console.log(this.longitude);
+      }
+    }
+
+    // createMarker(place) {
+    //   var marker = new google.maps.Marker({
+    //     map: map,
+    //     position: place.geometry.location
+    //   });
+    
+    //   google.maps.event.addListener(marker, 'click', function() {
+    //     infowindow.setContent(place.name);
+    //     infowindow.open(map, this);
+    //   });
  }
